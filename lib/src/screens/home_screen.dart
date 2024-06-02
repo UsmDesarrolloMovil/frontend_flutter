21d3be8dd80fca189ea0bc0df41770713d9c6dd4
Cristobal-coding <agartkg95@gmail.com>
2024-06-02 12:49:31 -0400

    home finished

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,31 +1,52 @@
-import 'dart:math';
-
 import 'package:esports_app/src/widgets/custom_filled_button.dart';
 import 'package:esports_app/src/widgets/gradient_scaffold.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
+import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int? selectedContainer;
+
+  void changeSelection(int containerReference) {
+    setState(() {
+      selectedContainer = containerReference;
+    });
+  }
+
+  int? getSelection() {
+    return selectedContainer;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const GradientScaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _Header(),
-            SizedBox(height: 30),
-            _Content(),
-            Spacer(),
-            CustomFilledButton(),
-            SizedBox(height: 15),
-          ],
-        ),
+    return GradientScaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const _Header(),
+          _Content(getSelection, changeSelection),
+          const Spacer(),
+          Visibility(
+            visible: selectedContainer != null,
+            child: CustomFilledButton(onPressed: () {
+              switch (getSelection()) {
+                case 0:
+                  context.push('/campeonatos');
+                case 1:
+                  context.push('/equipos');
+                default:
+              }
+            }),
+          ),
+          const SizedBox(height: 15),
+        ],
       ),
     );
   }
@@ -37,67 +58,103 @@ class _Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textStyles = Theme.of(context).textTheme;
-    return Column(
-      children: [
-        Text('Bienvenido a', style: textStyles.titleSmall),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              MdiIcons.trophy,
-              size: 35,
-              color: Colors.yellow[800],
-            ),
-            const SizedBox(width: 5),
-            Text(
-              'Sports',
-              style: textStyles.titleLarge,
-            ),
-          ],
-        ),
-        const SizedBox(height: 30),
-        Text(
-          'Mantente actualizado con puntajes, estadísticas, horarios y jugada por jugada en tiempo real de todas sus ligas y equipos favoritos. \n\n 👇🏻',
-          style: textStyles.bodyMedium,
-          textAlign: TextAlign.center,
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          Text('Bienvenido a', style: textStyles.titleSmall),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                MdiIcons.trophy,
+                size: 35,
+                color: Colors.yellow[800],
+              ),
+              const SizedBox(width: 5),
+              Text(
+                'Sports',
+                style: textStyles.titleLarge,
+              ),
+            ],
+          ),
+          const SizedBox(height: 30),
+          Text(
+            'Mantente actualizado con puntajes, estadísticas, horarios y jugada por jugada en tiempo real de todas sus ligas y equipos favoritos. \n\n 👇🏻',
+            style: textStyles.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30),
+          Text(
+            'Seleccione:',
+            style: textStyles.titleSmall,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
 
 class _Content extends StatelessWidget {
-  const _Content({super.key});
+  final int? Function() getSelection;
+  final void Function(int) setSelection;
+  const _Content(this.getSelection, this.setSelection);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final selection = getSelection();
     final colors = Theme.of(context).colorScheme;
-    return Expanded(
+    final List<BoxShadow> boxshadow = [
+      BoxShadow(
+        color: colors.onError,
+        offset: const Offset(0, 0),
+        blurRadius: 16,
+        spreadRadius: 4,
+      )
+    ];
+    return SizedBox(
+      width: size.width,
+      height: size.height * 0.3,
       child: ListView(
+        physics: NeverScrollableScrollPhysics(),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
         scrollDirection: Axis.horizontal,
         children: [
-          Container(
-            width: size.width * 0.4,
-            height: size.width * 0.5,
-            decoration: BoxDecoration(
-              color: colors.background,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: const Center(
-              child: Text('Campeonatos'),
+          InkResponse(
+            radius: 50,
+            splashColor: colors.onError,
+            onTap: () => setSelection(0),
+            child: Container(
+              width: size.width * 0.45,
+              height: size.width * 0.5,
+              decoration: BoxDecoration(
+                color: colors.background,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: selection != null && selection == 0 ? boxshadow : [],
+              ),
+              child: const Center(
+                child: Text('Campeonatos'),
+              ),
             ),
           ),
           const SizedBox(width: 20),
-          Container(
-            width: size.width * 0.4,
-            height: size.width * 0.5,
-            decoration: BoxDecoration(
-              color: colors.background,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: const Center(
-              child: Text('Equipos'),
+          InkResponse(
+            radius: 50,
+            splashColor: colors.onError,
+            onTap: () => setSelection(1),
+            child: Container(
+              width: size.width * 0.45,
+              height: size.width * 0.5,
+              decoration: BoxDecoration(
+                color: colors.background,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: selection != null && selection == 1 ? boxshadow : [],
+              ),
+              child: const Center(
+                child: Text('Equipos'),
+              ),
             ),
           ),
         ],
--- lib/src/widgets/custom_filled_button.dart
+++ lib/src/widgets/custom_filled_button.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:animate_do/animate_do.dart';
 
 class CustomFilledButton extends StatelessWidget {
-  const CustomFilledButton({super.key});
+  final VoidCallback onPressed;
+  const CustomFilledButton({super.key, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
     Size size = MediaQuery.of(context).size;
     return SlideInUp(
-      delay: const Duration(milliseconds: 400),
+      delay: const Duration(milliseconds: 200),
       child: SizedBox(
         width: size.width,
         child: FilledButton(
@@ -21,7 +22,7 @@ class CustomFilledButton extends StatelessWidget {
             ),
             padding: const EdgeInsets.symmetric(vertical: 17),
           ),
-          onPressed: () {},
+          onPressed: onPressed,
           child: const Text('Continuar'),
         ),
       ),
